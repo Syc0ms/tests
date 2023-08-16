a794456001a7caefc8dc770f4bdec019a3d6b811
Ben <babaxanyanbenjamin@gmail.com>
2023-08-16 18:19:40 +0400

    ad

--- mail.php
+++ mail.php
@@ -1,72 +1,31 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $firstName = $_POST["firstName"];
+    $lastName = $_POST["lastName"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $ext = $_POST["ext"];
 
-require '../vendor/autoload.php';
+    $subject = "Event Form Submission";
 
-$firstName = $_POST["firstName"];
-$lastName = $_POST["lastName"];
-$phone = $_POST["phone"];
-$email = $_POST["email"];
+    $message = "First Name: $firstName\n";
+    $message .= "Last Name: $lastName\n";
+    $message .= "Email: $email\n";
+    $message .= "Phone Number: $phone ext. $ext\n";
 
-$mail = new PHPMailer();
+    // Additional fields from the form can be added to the $message
 
-$mail->isSMTP();
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "X-Mailer: PHP/" . phpversion();
 
-$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    $to = "babaxanyanbenjamin@gmail.com"; // Replace with the recipient's email address
 
-$mail->Host = 'smtp.gmail.com';
-$mail->Port = 465;
-
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-$mail->SMTPAuth = true;
-
-//Username to use for SMTP authentication - use full email address for gmail
-$mail->Username = 'babaxanyanbenjamin@gmail.com';
-
-//Password to use for SMTP authentication
-$mail->Password = 'nelizaben10';
-
-//Set who the message is to be sent from
-//Note that with gmail you can only use your account address (same as `Username`)
-//or predefined aliases that you have configured within your account.
-//Do not use user-submitted addresses in here
-$mail->setFrom('babaxanyanbenjamin@gmail.com', 'First Last');
-
-//Set an alternative reply-to address
-//This is a good place to put user-submitted addresses
-//Set who the message is to be sent to
-$mail->addAddress('babaxanyanben1@gmail.com', 'John Doe');
-
-//Set the subject line
-$mail->Subject = 'PHPMailer GMail SMTP test';
-
-//Read an HTML message body from an external file, convert referenced images to embedded,
-//convert HTML into a basic plain-text alternative body
-//Replace the plain text body with one created manually
-$mail->AltBody = $phone;
-
-if (!$mail->send()) {
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-} else {
-    echo 'Message sent!';
-    //Section 2: IMAP
-    //Uncomment these to save your message in the 'Sent Mail' folder.
-    #if (save_mail($mail)) {
-    #    echo "Message saved!";
-    #}
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Email sent successfully!";
+    } else {
+        echo "Email sending failed.";
+    }
 }
+?>
 
-function save_mail($mail)
-{
-    //You can change 'Sent Mail' to any other folder or tag
-    $path = '{imap.gmail.com:993/imap/ssl}[Gmail]/Sent Mail';
-
-    //Tell your server to open an IMAP connection using the same username and password as you used for SMTP
-    $imapStream = imap_open($path, $mail->Username, $mail->Password);
-
-    $result = imap_append($imapStream, $path, $mail->getSentMIMEMessage());
-    imap_close($imapStream);
-
-    return $result;
-}
